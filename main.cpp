d086953b09c99a1ecaa44ac4e0598a496eacd3e0
jashworth-isb <justin.ashworth@systemsbiology.org>
2014-08-20 12:53:59 -0700

    substituting atof for unsupported stof function

--- main.cpp
+++ main.cpp
@@ -62,7 +62,7 @@ void pearson_distances(Matrix const & matrix, Indices const & colinds, t_float *
 				EYY += y*y;
 				EXY += x*y;
 			}
-			
+
 			d = 2.0;
 			if(npairs>0){
 				// nan values can appear during bootstrap resampling, due to repeated values and a zero denominator
@@ -70,7 +70,7 @@ void pearson_distances(Matrix const & matrix, Indices const & colinds, t_float *
 				denom = (EXX - EX*EX/npairs)*(EYY - EY*EY/npairs);
 				if(denom>0) d = 1.0 - (EXY - EX*EY/npairs) / sqrt(denom);
 			}
-			
+
 			//std::cerr << d << std::endl;
 			if(fc_isnan(d)){
 				std::cerr << "nan distance value for r1 " << r1 << " r2 " << r2 << " EX " << EX << " EY " << EY << " EXX " << EXX << " EYY " << EYY << " EXY " << EXY << " npairs " << npairs << " num " << num << " denom " << denom << " sqrtdenom " << sqrt(denom) << std::endl;
@@ -113,7 +113,7 @@ typedef std::set<unsigned> Ties;
 // 1) it's c++, 2) it precomputes all ranks prior to N^2 loop.
 // handles missing data ('NAs') in a pairwise.complete.obs fashion
 void spearman_distances(Matrix const & matrix, Indices const & colinds, t_float * const dist){
-	
+
 	// precompute ranks for all rows
 	size_t const nrow(matrix.size()), ncol(colinds.size());
 	Matrix ranks(nrow);
@@ -130,12 +130,12 @@ void spearman_distances(Matrix const & matrix, Indices const & colinds, t_float 
 			++colcounter;
 		}
 		std::sort(ordvals.begin(), ordvals.end(), sortbyval);
-		
+
 		Indices rankorder;
 		for(size_t i(0); i<ordvals.size(); ++i) rankorder.push_back(ordvals[i].orig_ind);
 		//        std::copy(rankorder.begin(), rankorder.end(), std::ostream_iterator<size_t>(std::cout," "));
 		//        std::cout << std::endl;
-		
+
 		// compute ranks
 		Values rowranks;
 		Ties ties;
@@ -151,9 +151,9 @@ void spearman_distances(Matrix const & matrix, Indices const & colinds, t_float 
 			}
 			// ranks are 1-indexed (though this shouldn't matter?)
 			else rowranks.push_back(vali+1);
-			
+
 			//std::cout << i << " " << vali+1 << " " << xi << " " << x << " " << yi << " " << y << std::endl;
-			
+
 			// deal with ties
 			if(val == last){
 				ties.insert(vali+1);
@@ -165,12 +165,12 @@ void spearman_distances(Matrix const & matrix, Indices const & colinds, t_float 
 				for(size_t ip(0); ip<ties.size(); ++ip) rowranks[i-ip-1] = rank;
 				ties.clear();
 			}
-			
+
 			last = val;
 			++vali;
 		}
 		// std::cout << xranks.size() << std::endl;
-		
+
 		// process final ties
 		if(!ties.empty()){
 			t_float rank(0);
@@ -178,12 +178,12 @@ void spearman_distances(Matrix const & matrix, Indices const & colinds, t_float 
 			rank /= ties.size();
 			for(size_t ip(0); ip<ties.size(); ++ip) rowranks[vali-ip-1] = rank;
 		}
-		
+
 		// now 'unsort' the ranks so that any pair of two ranks will re-correspond to each other
 		ranks[row].resize(rankorder.size());
 		for(size_t i(0); i<rankorder.size(); ++i) ranks[row][rankorder[i]] = rowranks[i];
 	}
-	
+
 	// pairwise pearson correlations of the pre-computed spearman ranks
 	std::ptrdiff_t p(0);
 	t_float EX(0.0), EY(0.0), EXX(0.0), EYY(0.0), EXY(0.0), x(0.0), y(0.0), d(0.0), num(0.0), denom(0.0);
@@ -191,7 +191,7 @@ void spearman_distances(Matrix const & matrix, Indices const & colinds, t_float 
 	unsigned npairs(0);
 	for(r1=0; r1<(nrow-1); ++r1){
 		for(r2=r1+1; r2<nrow; ++r2){
-					
+
 			EX=0, EY=0, EXX=0, EYY=0, EXY=0;
 			npairs=0;
 			for(i=0; i<ncol; ++i){
@@ -205,7 +205,7 @@ void spearman_distances(Matrix const & matrix, Indices const & colinds, t_float 
 				EYY += y*y;
 				EXY += x*y;
 			}
-			
+
 			d = 2.0;
 			if(npairs>0){
 				// nan values can appear during bootstrap resampling, due to repeated values and a zero denominator
@@ -213,14 +213,14 @@ void spearman_distances(Matrix const & matrix, Indices const & colinds, t_float 
 				denom = (EXX - EX*EX/npairs)*(EYY - EY*EY/npairs);
 				if(denom>0) d = 1.0 - (EXY - EX*EY/npairs) / sqrt(denom);
 			}
-			
+
 			//std::cerr << d << std::endl;
 			if(fc_isnan(d)){
 				std::cerr << "nan distance value for r1 " << r1 << " r2 " << r2 << " EX " << EX << " EY " << EY << " EXX " << EXX << " EYY " << EYY << " EXY " << EXY << " npairs " << npairs << " num " << num << " denom " << denom << " sqrtdenom " << sqrt(denom) << std::endl;
 			}
-			
+
 			dist[p++] = d;
-			
+
 		}
 	}
 }
@@ -238,7 +238,7 @@ void read_matrix_file(
 		exit(EXIT_FAILURE);
 	}
 	std::cerr << "Reading file " << filename << std::endl;
-	
+
 	// read input file
 	std::string line;
 	bool firstline(true);
@@ -261,11 +261,12 @@ void read_matrix_file(
 			// set any "NA"s to a magic big number to keep track later
 			t_float value;
 			if(token=="NA") value=NANVALUE;
-			else value=std::stof(token);
+//			else value=std::stof(token); // this is c++11 (not always available)
+			else value=std::atof(token.c_str());
 			//			std::cerr << value << std::endl;
 			ratios.push_back(value);
 		}
-		
+
 		rr.push_back(ratios);
 	}
 }
@@ -279,7 +280,7 @@ void read_merge_file(std::string const & filename, Merge & merge){
 		exit(EXIT_FAILURE);
 	}
 	std::cerr << "Reading file " << filename << std::endl;
-	
+
 	// read input file
 	std::string line;
 	while (getline(file, line)) {
@@ -304,7 +305,7 @@ void get_members(Merge const & merge, Members & members){
 		if(ai<0) {members[i].clear(); members[i].push_back(-1*ai);}
 		// note: indices in R and fastcluster's merge table are 1-indexed
 		else members[i] = members[ai-1];
-		
+
 		ai = merge[i][1];
 		if(ai<0) members[i].push_back(-1*ai);
 		else members[i].insert(members[i].end(),members[ai-1].begin(),members[ai-1].end());
@@ -318,11 +319,11 @@ void get_members(Merge const & merge, Members & members){
 void add_equal_nodes(Merge const & merge1, Merge const & merge2, Counts & nodecounts){
 	// merge is a two-column matrix of integers from hclust
 	size_t const nrow1(merge1.size());
-	
+
 	Members memb1(merge1.size()), memb2(merge2.size());
 	get_members(merge1, memb1);
 	get_members(merge2, memb2);
-	
+
 	// assumes unique members and does not re-compare to previously matched ones
 	std::vector<bool> matched(nrow1,false);
 	for(size_t i1(0); i1<memb1.size(); ++i1){
@@ -348,7 +349,7 @@ void add_equal_nodes(Merge const & merge1, Merge const & merge2, Counts & nodeco
 void output_bootstrap_results(HclustResults const & bootstraps, std::string prefix="bootstrap."){
 	// outputs that can be read into R
 	std::ofstream of;
-	
+
 	std::string fname = prefix+"labels";
 	of.open(fname.c_str());
 	for(HclustResults::const_iterator bs(bootstraps.begin()); bs!=bootstraps.end(); ++bs){
@@ -356,7 +357,7 @@ void output_bootstrap_results(HclustResults const & bootstraps, std::string pref
 		of << '\n';
 	}
 	of.close();
-	
+
 	fname = prefix+"merges";
 	of.open(fname.c_str());
 	for(HclustResults::const_iterator bs(bootstraps.begin()); bs!=bootstraps.end(); ++bs){
@@ -366,7 +367,7 @@ void output_bootstrap_results(HclustResults const & bootstraps, std::string pref
 		of << '\n';
 	}
 	of.close();
-	
+
 	fname = prefix+"heights";
 	of.open(fname.c_str());
 	for(HclustResults::const_iterator bs(bootstraps.begin()); bs!=bootstraps.end(); ++bs){
@@ -374,7 +375,7 @@ void output_bootstrap_results(HclustResults const & bootstraps, std::string pref
 		of << '\n';
 	}
 	of.close();
-	
+
 	fname = prefix+"orders";
 	of.open(fname.c_str());
 	for(HclustResults::const_iterator bs(bootstraps.begin()); bs!=bootstraps.end(); ++bs){
@@ -382,7 +383,7 @@ void output_bootstrap_results(HclustResults const & bootstraps, std::string pref
 		of << '\n';
 	}
 	of.close();
-	
+
 }
 
 void output_node_counts(Counts const & nodecounts, std::string prefix=""){
@@ -395,13 +396,13 @@ void output_node_counts(Counts const & nodecounts, std::string prefix=""){
 
 void output_hclust(HclustResult const & result, std::string prefix=""){
 	std::ofstream of;
-	
+
 	std::string fname = prefix+"labels";
 	of.open(fname.c_str());
 	std::copy(result.labels.begin(), result.labels.end(), std::ostream_iterator<std::string>(of, " "));
 	of << '\n';
 	of.close();
-	
+
 	fname = prefix+"merge";
 	of.open(fname.c_str());
 	for(size_t j(0); j<2; ++j){
@@ -409,29 +410,29 @@ void output_hclust(HclustResult const & result, std::string prefix=""){
 	}
 	of << '\n';
 	of.close();
-	
+
 	fname = prefix+"height";
 	of.open(fname.c_str());
 	std::copy(result.height.begin(), result.height.end(), std::ostream_iterator<t_float>(of, " "));
 	of << '\n';
 	of.close();
-	
+
 	fname = prefix+"order";
 	of.open(fname.c_str());
 	std::copy(result.order.begin(), result.order.end(), std::ostream_iterator<int>(of, " "));
 	of << '\n';
 	of.close();
-	
+
 }
 
 void run_fastcluster(HclustResult & hclust_result, t_float * dist, int method){
 	//std::cout << "Fastcluster..." << std::endl;
-	
+
 	if (method<METHOD_METR_SINGLE || method>METHOD_METR_MEDIAN) {
 		std::cerr << "ERROR: unknown clustering method requested" << std::endl;
 		exit(EXIT_FAILURE);
 	}
-	
+
 	const int N((int)hclust_result.labels.size());
 	// 'members': members in each node (for 'clustering in the middle of the tree')
 	// here, just setting this whole thing to 1 (all data are for terminal branches)
@@ -439,9 +440,9 @@ void run_fastcluster(HclustResult & hclust_result, t_float * dist, int method){
 	members.init(N);
 	for (int i(0); i<N; ++i) members[i] = 1;
 	cluster_result hc(N-1);
-	
+
 	switch (method) {
-			
+
 		case METHOD_METR_SINGLE:
 			MST_linkage_core(N, dist, hc);
 			break;
@@ -467,19 +468,19 @@ void run_fastcluster(HclustResult & hclust_result, t_float * dist, int method){
 			std::cerr << "ERROR: unknown clustering method requested" << std::endl;
 			exit(EXIT_FAILURE);
 	}
-	
+
 	// here init vector sizes and pass array pointers to fastcluster
 	hclust_result.height.resize(N-1);
 	hclust_result.order.resize(N);
-	
+
 	Ints merge(2*(N-1),0);
-	
+
 	if (method==METHOD_METR_CENTROID ||
 	    method==METHOD_METR_MEDIAN)
 		generate_R_dendrogram<true>(&merge[0], &hclust_result.height[0], &hclust_result.order[0], hc, N);
 	else
 		generate_R_dendrogram<false>(&merge[0], &hclust_result.height[0], &hclust_result.order[0], hc, N);
-	
+
 	// convert merge (flat array) into two-column Merge matrix for later convenience/intelligibility
 	hclust_result.merge.resize(N-1);
 	for(size_t i(0); i<N-1; ++i){
@@ -487,9 +488,9 @@ void run_fastcluster(HclustResult & hclust_result, t_float * dist, int method){
 		// merge array is filled by column
 		for(size_t j(0); j<2; ++j) hclust_result.merge[i][j] = merge[i+(N-1)*j];
 	}
-	
+
 	members.free();
-	
+
 	/*
 	 //debug check for proper merge conversion to STL container
 	 for(size_t i(0); i<hclust_result.merge.size(); ++i){
@@ -527,140 +528,140 @@ void usage_error(){
 
 ////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[]) {
-	
+
 	std::srand((unsigned int)time(NULL));
-	
+
 	std::cout << std::endl;
-	
+
 	std::string ratiosfilename, distmethod;
 	unsigned bootstraps(0);
 	int method(4); // ward
 	double scale(1.0); // ratio of columns to resample for multiscale bootstrapping
-	
+
 	if (argc < 2) usage_error();
-	
+
 	// parse command line arguments
 	for (int i(1); i < argc; ++i) {
-		
+
 		std::string arg(argv[i]);
-		
+
 		if (arg == "-r" || arg == "--ratios") {
 			if (++i >= argc) usage_error();
 			ratiosfilename = argv[i];
-			
+
 		} else if (arg == "-b" || arg == "--bootstraps") {
 			if (++i >= argc) usage_error();
 			bootstraps = atoi(argv[i]);
-			
+
 		} else if (arg == "-s" || arg == "--scale") {
 			if (++i >= argc) usage_error();
 			scale = atof(argv[i]);
-			
+
 		} else if (arg == "-m" || arg == "--method") {
 			if (++i >= argc) usage_error();
 			method = atoi(argv[i]);
-			
+
 		} else if (arg == "-d" || arg == "--distance") {
 			if (++i >= argc) usage_error();
 			distmethod = argv[i];
-			
+
 		} else if (arg == "-h" || arg == "--help") usage_error();
 	}
-	
+
 	// read in matrix
 	Matrix rr;
 	Labels labels;
 	read_matrix_file(ratiosfilename, rr, labels);
-	
+
 	//size_t ntest(std::min(size_t(5),rr.size()));
 	//size_t nprint(ntest);
 	//	for(size_t i(0); i<ntest; ++i){ std::cout << ids[i] << " " << rr[i] << std::endl; }
-	
+
 	const size_t nrow(rr.size()), ncol(rr.front().size());
-	
+
 	// dinstance func expects column indices, here just feed 1:ncol (becomes useful for bootstrap resampling)
 	Indices colinds;
 	for(size_t i(0); i<rr.front().size(); ++i) colinds.push_back(i);
-	
+
 	// Parameter NN: number of non-redundant, non-self comparisons
 	const std::ptrdiff_t NN = static_cast<std::ptrdiff_t>((nrow)*(nrow-1)/2);
 	std::cout << "NN is " << NN << std::endl;
 	std::cout << "Distance matrix..." << std::endl;
 	auto_array_ptr<t_float> dist;
 	dist.init(NN);
-	
+
 	// feed whole matrix to distance functions:
 	// for Spearman, this way can pre-compute ranks prior to the N^2 loop
 	get_distances(rr, colinds, dist, distmethod);
-	
+
 	// here: do a non-bootstrapped hclust
 	HclustResult result;
 	// store labels
 	for(size_t i(0); i<labels.size(); ++i) result.labels.push_back(labels[i]);
-	
+
 	run_fastcluster(result, dist, method);
-	
+
 	// output result
 	output_hclust(result,"hc.");
-	
+
 	// bootstrap iterations
 	HclustResults bootstrap_results;
 	Counts nodecounts(result.merge.size(),0);
-	
+
 	std::cout << "Bootstrap iterations..." << std::endl;
 	unsigned nsample(ncol*scale);
 	for(unsigned bs(0); bs<bootstraps; ++bs){
-		
+
 		HclustResult bs_result;
 		// store ids
 		for(size_t i(0); i<labels.size(); ++i) bs_result.labels.push_back(labels[i]);
-		
+
 		// resample columns (e.g. microarray conditions) with replacement
 		Indices colinds;
 		for(t_index i(0); i<nsample; ++i) colinds.push_back(rand() % ncol);
-		
+
 		// this sort probably not necessary
 		//std::sort(colinds.begin(), colinds.end());
-		
+
 		/*std::cerr << "Colinds: ";
 		 for(t_index i(0); i<nsample; ++i) std::cerr << colinds[i] << " ";
 		 std::cerr << std::endl;*/
-		
+
 		// resample from columns and create a new distance matrix
 		auto_array_ptr<t_float> dist_resampled;
 		dist_resampled.init(NN);
-		
+
 		get_distances(rr, colinds, dist_resampled, distmethod);
 		run_fastcluster(bs_result, dist_resampled, method);
 		dist_resampled.free();
-		
+
 		//bootstrap_results.push_back(bs_result);
-		
+
 		// pvclust-style equal node counting
 		add_equal_nodes(result.merge, bs_result.merge, nodecounts);
-		
+
 		// cache current counts
 		// outputs that can be read into R
 		std::ofstream of;
-		
+
 		std::string fname = "countscurrent";
 		of.open(fname.c_str());
 		for(Counts::const_iterator it(nodecounts.begin()); it!=nodecounts.end(); ++it) of << *it << '\n';
 		of.close();
-		
+
 		fname = "lastiter";
 		of.open(fname.c_str());
 		of << bs << '\n';
 		of.close();
-		
+
 		std::cout << '.' << std::flush;
-		
+
 	}
 	std::cout << std::endl;
-	
+
 	output_bootstrap_results(bootstrap_results);
 	output_node_counts(nodecounts);
-	
+
 	// maybe in the future: height-based agglomeration to yield k clusters (see dendextendRcpp?),
 	// empirical distributions of pairwise cluster co-memberships for many choices of k
 	std::cout << "Finished." << std::endl;
