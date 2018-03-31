94a2f60e270e453627e5fd8788a15891bd27397e
JA <justin.ashworth@uts.edu.au>
2018-03-31 12:16:14 +1100

    .

--- hclust.from.fcbs.R
+++ hclust.from.fcbs.R
@@ -3,12 +3,16 @@ hc_from_fcbs = function(hcfile='hc',dist.method='',hclust.method='fastcluster'){
 	# convert output files from c++ fastcluster mod into R hclust object
 	hc = list()
 	class(hc) = 'hclust'
-	hc$merge =  as.integer( unlist( strsplit(readLines( sprintf('%s.merge',hcfile))[1],' ')))
-	hc$height = as.numeric( unlist( strsplit(readLines( sprintf('%s.height',hcfile))[1],' ')))
-	hc$order =  as.integer( unlist( strsplit(readLines( sprintf('%s.order',hcfile))[1],' ')))
-	hc$labels =             unlist( strsplit(readLines( sprintf('%s.labels',hcfile))[1],' '))
+	hc$merge =  as.integer( unlist( strsplit(readLines( sprintf('%s.merge',hcfile)),' ')))
+	hc$height = as.numeric( unlist( strsplit(readLines( sprintf('%s.height',hcfile)),' ')))
+	hc$order =  as.integer( unlist( strsplit(readLines( sprintf('%s.order',hcfile)),' ')))
+	hc$labels =             unlist( strsplit(readLines( sprintf('%s.labels',hcfile)),' '))
 
 	dim(hc$merge) = c(length(hc$merge)/2, 2)
+	if(nrow(hc$merge) != length(hc$height)){
+		cat('ERROR: merges/heights length mismatch!!\n')
+		return(NULL)
+	}
 	hc$labels = gsub('\"','',hc$labels)
 	hc$method = hclust.method
 	hc$call = match.call()
--- pvclust.post.R
+++ pvclust.post.R
@@ -4,16 +4,29 @@ source('plot_pvdend.R')
 source('dendindexed.R')
 require(pvclust)
 require(fastcluster)
+require(dendextendRcpp)
 
 mindend = 2
 maxdend = 2000
 
-expylab = "Transcript level (CPM)"
+ylab1 = "Microarray (log2 FC)"
+ylab2 = "RNAseq (log10 TPM)"
 
-if(!exists('dd')){
+plots = F
+do_uncontained = F
+plot_all_pvclusters = F
+
+if(!exists('dd') & (plots | plot_all_pvclusters)){
 	rfile = 'data'
 	cat('reading data',rfile,'\n')
 	dd = read.table(rfile)
+	dd = as.matrix(dd)
+	dd[dd==-Inf] = NA
+	dd[dd==Inf] = NA
+
+	separatecols = c()
+	if(file.exists('separatecols')) separatecols = readLines('separatecols')
+	sepcols = colnames(dd) %in% separatecols
 }
 
 cache = c('pvc','hpk','cls')
@@ -39,11 +52,14 @@ hc = pvc$hclust
 
 if(!exists('clusters')){
 	pt='au'
+#	pvcut=0.90
 	pvcut=0.80
 	cat(sprintf('pvpick with P[%s]>=%f\n',pt,pvcut))
 	clpick = pvpick(pvc,pv=pt,alpha=pvcut,max.only=T)
 	save(clpick,file='pvpick.RData')
 	clusters = clpick$clusters
+	pvcsizes = sapply(clusters, function(x){length(x)})
+	cat(sprintf('%i pvclust clusters, max size %i, median size %i\n', length(clusters), max(pvcsizes), round(median(pvcsizes)) ))
 }
 
 } else {
@@ -58,16 +74,12 @@ if(exists('clusters')){
 	ordlabs = hc$labels[ hc$order ]
 }
 
-plots = T
-if(plots){
-
 if(!exists('hpk')){
 	hpk=get_hpk(hc,0.8)
 	save(hpk,file='hpk.RData')
 }
 
 if(!exists('cls')){
-require(dendextendRcpp)
 ncl=1000
 if(!ncl %in% names(hpk)){
 	ncls = as.numeric(names(hpk))
@@ -79,13 +91,47 @@ save(cls,file='cls.RData')
 }
 
 exp_plot = function(expvals, desc=NULL, ...){
-	log='y'
-	par(mar=c(10,4,6,3))
+	if(all(is.na(expvals))) return()
+	log=''
+#	log='y'
+	par(mar=c(10,4,6,5))
 	ids = rownames(expvals)
 	cols = rainbow(length(ids))
-	matplot( t(expvals), type='n', xaxt='n', xaxs='i', log=log, ...)
-	matlines( t(expvals), lty=1, lwd=2, col=cols)
-	axis(1, las=2, at=1:ncol(expvals), labels=colnames(expvals), cex.axis=0.5)
+
+	# if we have a combination of separate datasets (e.g. array and rnaseq data) in the data matrix, plot separately
+	if(any(sepcols) & (length(which(sepcols)) < ncol(expvals))){
+
+		ylim = range(expvals[,sepcols],na.rm=T)
+		# R's ylim returns Infs if ALL values are NA
+		if(all(is.na(expvals[,sepcols])) | all(is.na(ylim))) ylim = c(-1,1)
+		plot(NA, xlim=c(0,ncol(expvals)), ylim=ylim, type='n', xlab='', ylab=ylab1, xaxt='n', xaxs='i', log=log, ...)
+
+		for(r in 1:nrow(expvals)){
+			lines(1:length(which(sepcols)),expvals[r,sepcols],col=cols[r], lwd=2, lty=1)
+		}
+		axis(1, las=2, at=1:length(which(sepcols)), labels=colnames(expvals)[sepcols], cex.axis=0.5)
+
+		abline(v=length(which(sepcols))+0.5,lty=2,col=rgb(0,0,0,0.4))
+
+		par(new=T)
+		ylim = range(expvals[,!sepcols],na.rm=T)
+		# R's ylim returns Infs if ALL values are NA
+		if(all(is.na(expvals[,!sepcols])) | all(is.na(ylim))) ylim = c(-1,1)
+		plot(NA, xlim=c(0,ncol(expvals)), ylim=ylim, type='n', yaxt='n',xlab='', ylab='', xaxt='n', xaxs='i', log=log, ...)
+
+		for(r in 1:nrow(expvals)){
+			lines((length(which(sepcols))+1):length(sepcols),expvals[r,!sepcols],col=cols[r], lwd=2, lty=1)
+		}
+		axis(1, las=2, at=(length(which(sepcols))+1):length(sepcols), labels=colnames(expvals)[!sepcols], cex.axis=0.5)
+		axis(4, pretty(range(expvals[,!sepcols],na.rm=T)))
+		mtext(ylab2,side=4,line=3)
+
+	} else {
+		matplot( t(expvals), type='n', xaxt='n', xaxs='i', log=log, ylab='Expression', ...)
+		matlines( t(expvals), lty=1, lwd=2, col=cols)
+		axis(1, las=2, at=1:ncol(expvals), labels=colnames(expvals), cex.axis=0.5)
+	}
+
 	if(!is.null(desc)){
 		for(i in 1:length(ids)){
 			mtext(ids[i], line=-1*i, col=cols[i])
@@ -132,45 +178,29 @@ plot_exp = function(values,fname,...){
 	dev.off()
 }
 
-desctab = read.delim('desc')
 clstab = as.data.frame( do.call(rbind, lapply(1:length(cls), function(i){ cbind(i,labels(cls[[i]]))})))
 names(clstab) = c('cl','id')
 pvctab = as.data.frame( do.call(rbind, lapply(1:length(clpick$clusters), function(i){ cbind(i,clpick$clusters[[i]])})))
 names(pvctab) = c('pvc','id')
 m = merge(clstab, pvctab, by='id', all.x=T, sort=F)
-m = merge(m, desctab, by='id', all.x=T, sort=F)
-hc_for_id = as.numeric(as.character(m$cl))
-names(hc_for_id) = as.character(m$id)
 m = m[order(m$cl,m$pvc,m$id),]
 tsv(m,'cls.tsv')
 
-#warning: tapply sorts by as.character(index) here:
-clids = tapply(m$id,m$cl,list)
-# sort numerically so list index == cluster index
-clids = clids[ order(as.numeric(names(clids))) ]
-
-# convenient lists of image files corresponding to larger bootstrap clusters
-pvclens = sapply(clpick$clusters, length)
-minlen = 9
-cat( sprintf('pvc/exp.%04d.pdf', which( pvclens > minlen )), sep='\n', file='pvc_big_exp')
-hc_for_pvc = m$cl
-names(hc_for_pvc) = m$pvc
-hc_big_pvcs = unique( hc_for_pvc[ pvclens > minlen ] )
-cat( sprintf('dendro/dendro.%04d.pdf', hc_big_pvcs), sep='\n', file='hc_big_pvcs_dendro')
-cat( sprintf('exp/exp.%04d.pdf', hc_big_pvcs), sep='\n', file='hc_big_pvcs_exp')
-
-logcols = c()
-if(file.exists('logcols')) logcols = readLines('logcols')
-
-plot_dends=T
-if(plot_dends){
-	desc = paste(desctab$id,desctab$desc)
-	names(desc) = as.character(desctab$id)
-	# this may be necessary if the transcripts clustered have '.t1' suffices, but the annotation ids don't
+#desctab = read.delim('desc')
+#desctab = unique(desctab)
+#m = merge(m, desctab, by='id', all.x=T, sort=F)
+#tsv(m,'cls.tsv')
+#desc = paste(desctab$id,desctab$desc)
+#names(desc) = as.character(desctab$id)
+# this may be necessary if the transcripts clustered have '.t1' suffices, but the annotation ids don't
 #	names(desc) = sprintf('%s.t1',names(desc))
 
+hc_for_id = as.numeric(as.character(m$cl))
+names(hc_for_id) = as.character(m$id)
+
+if(plots){
 	baseh = 480
-	denddir = 'dendro'
+	denddir = 'dnd'
 	dir.create(denddir)
 	expdir = 'exp'
 	dir.create(expdir)
@@ -203,19 +233,17 @@ if(plot_dends){
 		fname = sprintf('%s/exp.%04d.pdf',expdir,i)
 		pdf(sprintf(fname,expdir,i),height=8,width=20,colormodel='cmyk')
 		main = sprintf('Cluster %i: expression values',i)
-		exp_plot(dd[ids,],desc,main=main,ylab=expylab)
+		exp_plot(dd[ids,],desc,main=main)
 		dev.off()
 #		plot_exp(values,fname,main=main,ylab=expylab,xlab='')
 	}
-
 }
 
 # plot out all of the actual pvclust clusters themselves, agnostic of the hc height-based clustering
-plot_all_pvclusters=T
 if(plot_all_pvclusters){
 cat('Plotting all PVClust clusters\n')
 
-pvcdenddir = 'pvc_dendro'
+pvcdenddir = 'pvc_dnd'
 pvcexpdir = 'pvc_exp'
 
 dir.create(pvcdenddir)
@@ -230,7 +258,7 @@ invisible(dendrapply(dnd, function(x){
 			ids = labels(x)
 			sz = length(ids)
 			pickindex = which(clpick$edges==index)
-			cat('dendro',pickindex,sz,'transcripts\n')
+			cat('dnd',pickindex,sz,'transcripts\n')
 			if(sz<mindend){cat('cluster',pickindex,'too small (',sz,'genes)\n'); return()}
 			if(sz>maxdend){cat('cluster',pickindex,'too big (',sz,'genes)\n'); return()}
 			main = sprintf('Cluster %i',pickindex)
@@ -243,14 +271,14 @@ invisible(dendrapply(dnd, function(x){
 			# exp lineplot
 			pdf(sprintf('%s/pvcexp.%04d.pdf',pvcexpdir,pickindex),height=8,width=20,colormodel='cmyk')
 			main = sprintf('PVClust %i: expression values',pickindex)
-			exp_plot(dd[ids,],desc,main=main,ylab=expylab)
+			vals = dd[ids,]
+			if(!all(is.na(vals))) exp_plot(dd[ids,],desc,main=main)
 			dev.off()
 		}
 	}
 }))
 }
 
-do_uncontained = T
 if(do_uncontained & !exists('uncontained')){
 # figure out which pvpicked clusters are not contained within height-based clusters, and create output for them
 cat('adding significant pvclust clusters not already contained within height-based clusters\n')
@@ -270,10 +298,15 @@ save(uncontained,file='uncontained.clusters.RData')
 cat('\n')
 }
 
+# convenience lists for more post-processing
 allcls = lapply(cls, function(x){labels(x)})
+#warning: tapply sorts by as.character(index) here:
+clids = tapply(m$id,m$cl,list)
+# sort numerically so list index == cluster index
+clids = clids[ order(as.numeric(names(clids))) ]
+
 # add any uncontained pvclust clusters to the list of clusters
 #allcls = c(allcls, uncontained)
 
 #cluster_stats(dd,allcls)
 
-} # plots
